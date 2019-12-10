2c3111b330ccd522b146077fb5aad348c5253ab3
Thomas Abriat <abriat@et.esiea.fr>
2019-12-10 11:51:24 +0100

    Changement Map automatique Thomas

--- main.c
+++ main.c
@@ -17,26 +17,28 @@
 int main ( int argc, char** argv ){
     //initialisation des variables
     TTF_Init(); //on initialise les polices
-
     SDL_Init(SDL_INIT_VIDEO); //initialisation de la sdl
-    SDL_WM_SetIcon(IMG_Load("images/icon.png"), NULL); // Chargement de l'icône AVANT SDL_SetVideoMode
+    SDL_WM_SetIcon(IMG_Load("icon.png"), NULL); // Chargement de l'icône AVANT SDL_SetVideoMode
     SDL_Surface *ecran = NULL; //Surface sur laquelle on affichera les différents éléments
-    ecran = SDL_SetVideoMode((1280), (720), 32, SDL_HWSURFACE | SDL_DOUBLEBUF); //on affiche un écran de la taille souhaitée
+    ecran = SDL_SetVideoMode((1280), (720), 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_RESIZABLE); //on affiche un écran de la taille souhaitée
     SDL_WM_SetCaption("PST v0.0", NULL);   //titre de la fenêtre
     SDL_EnableKeyRepeat(15, 50); //répétition des touches (millisecondes)
     //initialisation des maps
     Case **Map=createMap(mapPath); //On crée un tableau  de cases aux dimensions correspondantes au nombre de cases de la map
     FileDecors *fileDecors = initialiserFileDecors();
+    FilePorte *filePorte = initialiserFilePorte();
     int chargementMap = 0;
     chargerSpritesMap(); //chargement en mémoire des sprites de la Map
     //initialisation du personnage
     Perso perso;
     perso.numSprite = 0;
     chargerSpritesPerso(perso.numSprite,perso.Perso_Sprites);
+
     //la boucle suivante constitue le menu
     int continuer = 1;
     int Menu=0;
     SDL_Event event; //on crée un evenement
+
     while (continuer){
         Menu = menu(ecran);
         switch(Menu){
@@ -45,15 +47,10 @@ int main ( int argc, char** argv ){
             break;
             case 0: //Thomas
                 //chargement
-                    chargerDecors(mapPath, fileDecors);//on charge les décors
-                    chargementMap=loadMap(mapPath, Map);
-                    chargerCollisionsDecors(fileDecors, Map);
-                    //printf("Load ok \n");
-                    //SDL_Delay(1000);
-
+                    changeMap(numMap, Map, fileDecors, filePorte);
+                  //  afficherFilePorteTerm(filePorte);
 
                 //Affichage
-
                   //  displayMap(Map, ecran);
                   // afficherDecors(fileDecors, ecran);
                   //afficherFileTerm(fileDecors);
@@ -63,15 +60,16 @@ int main ( int argc, char** argv ){
                            }
                            printf("\n");
                   }*/
-                  deplacerPerso(perso,ecran , Map, fileDecors);
+                  deplacerPerso(perso,ecran , Map, fileDecors, filePorte);
 
                 //Libération de la mémoire
                 viderFileDecors(fileDecors);
-
+                viderFilePorte(filePorte);
                 //SDL_Delay(3500);
             break;
             case 1: //Alexis
 
+            return 0;
 
             break;
             case 2: //Camille
