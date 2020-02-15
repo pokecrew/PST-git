00039aa5a7c87496a101cdc0c2666e1021f83073
Thomas Abriat <abriat@et.esiea.fr>
2020-02-15 07:18:38 +0100

    Mise à jour système affichage et ajout de la route 1 et de jadielle.

--- game.c
+++ game.c
@@ -1,1 +1,124 @@
 #include"include.h" //inclusion des .h nécessaires regroupés dans le fichier include.h
+int lancerEvenement(Case **Map,int i, int j, Direction direction, Perso perso, SDL_Surface *ecran){
+  //printf("TEst EVENEMENT\n");
+  //printf("%d\n", Map[i][j].type);
+  switch(Map[i][j].type){
+    case 2: //Hautes herbes
+      lancementCombat(ecran);
+      return 1;
+    break;
+    case 3: //rampe vers le bas
+        if(direction == BAS){
+          return 1;
+        }
+        else{
+          return 0;
+        }
+    break;
+    case 4: //rampe vers la droite
+        if(direction == DROITE){
+          return 1;
+        }
+        else{
+          return 0;
+        }
+    break;
+    case 5: //rampe vers la gauche
+        if(direction == GAUCHE){
+          return 1;
+        }
+        else{
+          return 0;
+        }
+    break;
+    case 6: //rampe vers la droite ou le bas
+        if(direction == DROITE || direction == BAS){
+          return 1;
+        }
+        else{
+          return 0;
+        }
+    break;
+    case 7: //rampe vers la gauche ou le bas
+        if(direction == GAUCHE || direction == BAS){
+          return 1;
+        }
+        else{
+          return 0;
+        }
+    break;
+    case 8://bord eau coin supérieur gauche
+      if((direction == DROITE || direction == BAS) && surf == TRUE ){
+        //changer skin pour surf
+        return 1;
+      }
+      else{
+        return 0;
+      }
+    break;
+    case 9: //bord eau avec eau à droite
+      if(direction == DROITE  && surf == TRUE ){
+        //changer skin pour surf
+        return 1;
+      }
+      else{
+        return 0;
+      }
+    break;
+    case 10: //bord eau coin inférieur gauche
+        if((direction == DROITE || direction == HAUT) && surf == TRUE){
+          return 1;
+        }
+        else{
+          return 0;
+        }
+    break;
+    case 11://bord eau avec eau au dessus
+      if(direction == HAUT && surf == TRUE ){
+        //changer skin pour surf
+        return 1;
+      }
+      else{
+        return 0;
+      }
+    break;
+    case 12: //bord eau coin inférieur droit
+      if((direction == GAUCHE || direction == HAUT) && surf == TRUE ){
+        //changer skin pour surf
+        return 1;
+      }
+      else{
+        return 0;
+      }
+    break;
+    case 13: //bord eau avec eau à gauche
+        if(direction == GAUCHE && surf == TRUE){
+          return 1;
+        }
+        else{
+          return 0;
+        }
+    break;
+    case 14://bord eau coin supérieur droit
+      if((direction == GAUCHE || direction == BAS) && surf == TRUE ){
+        //changer skin pour surf
+        return 1;
+      }
+      else{
+        return 0;
+      }
+    break;
+    case 15://bord eau avec eau au dessous
+      if(direction == BAS && surf == TRUE ){
+        //changer skin pour surf
+        return 1;
+      }
+      else{
+        return 0;
+      }
+    break;
+    default:
+      return 1;
+    break;
+  }
+}
--- perso.h
+++ perso.h
@@ -1,19 +1,9 @@
 #ifndef DEF_PERSO
 #define DEF_PERSO
 
-typedef struct Perso Perso;
-
-//Structure des objets Case
-struct Perso{
-    int x;        //position sur les x (coin supérieur gauche) (nombre de case)
-    int y;        //position sur les y (coin supérieur gauche)(nombre de case)
-    int numSprite; //nom de l'image de la case
-    SDL_Rect position; //positions de la surface
-    SDL_Surface *Perso_Sprites[24];//Tableau des sprites du personnage
-};
-
 //Entrer ici les prototypes
 void chargerSpritesPerso(int numSpritePerso, SDL_Surface **Perso_Sprites);
-void deplacerPerso(Perso perso, SDL_Surface *ecran, Case ** Map, FileDecors *file, FilePorte *filePorte);
+void jeu(Perso perso, SDL_Surface *ecran, Case ** Map, FileDecors *file, FilePorte *filePorte);
+void deplacement(Case ** Map, Perso *perso, FileDecors *fileDecors, FilePorte *filePorte, Direction direction);
 int autoriserDeplacement(Case ** Map,Direction direction, Perso perso, SDL_Surface *ecran);
 #endif // DEF_PERSO
