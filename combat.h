d46223c3835cc17007c861efada52dd1e731b9be
Votre Nom <theo.boussiquet@orange.fr>
2020-03-06 15:50:20 +0100

    statistique, pv perdu, experience

--- combat.h
+++ combat.h
@@ -1,18 +1,7 @@
 #ifndef DEF_COMBAT
 #define DEF_COMBAT
 
-typedef struct Poke Poke;
 
-struct Poke{
-  char nom[25];
-  int PV;
-  int exp;
-  int niv;
-  int def;
-  int att;
-  int vit;
-  int id;
-};
 
 void combat(SDL_Surface *ecran);
 void poke_nom(Poke pokemon[20]);
@@ -20,7 +9,9 @@ void sauvage(SDL_Surface *poke[16]);
 void poke_alea(SDL_Surface *ecran, SDL_Surface *poke[16], Poke pokemon[20]);
 void my_pokemons();
 void changer_poke(SDL_Surface *ecran);
-int deroulement( SDL_Surface *ecran, Poke *j1, Poke *j2, int joueur);
-
-
+int deroulement( SDL_Surface *ecran, int joueur);
+int calcul_stat(Poke *poke);
+int calcul_exp_gagne(Poke poke);
+void ajout_exp(int exp_gagne);
+int calcul_pv_perdu(Poke attaquant, Poke defenseur);
 #endif // DEF_COMBAT
