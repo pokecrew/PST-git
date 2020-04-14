3b4f8e93d62af19957122c89cc9378ab06b46717
tommy94200 <abriat@et.esiea.fr>
2020-04-14 11:11:23 +0200

    maj branche thomas

--- main.c
+++ main.c
@@ -12,58 +12,6 @@
 #include "game.c"
 #include "combat.c"
 #include "son.c"
-<<<<<<< HEAD
-#include "intro.c"
-
-int main ( int argc, char** argv ){
-    //initialisation des variables
-    TTF_Init(); //on initialise les polices
-    SDL_Init(SDL_INIT_VIDEO); //initialisation de la sdl
-    loadsoundEffect();//on charge les effets sonores
-    SDL_WM_SetIcon(IMG_Load("icon.png"), NULL); // Chargement de l'icône AVANT SDL_SetVideoMode
-    SDL_Surface *ecran = NULL; //Surface sur laquelle on affichera les différents éléments
-    ecran = SDL_SetVideoMode((1280), (720), 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_RESIZABLE); //on affiche un écran de la taille souhaitée
-    SDL_WM_SetCaption("PST v0.1.1", NULL);   //titre de la fenêtre
-    SDL_EnableKeyRepeat(15, 50); //répétition des touches (millisecondes)
-    //initialisation des maps
-    Case **Map=createMap(mapPath); //On crée un tableau  de cases aux dimensions correspondantes au nombre de cases de la map
-    FileDecors *fileDecors = initialiserFileDecors();
-    FilePorte *filePorte = initialiserFilePorte();
-    int chargementMap = 0;
-    chargerSpritesMap(); //chargement en mémoire des sprites de la Map
-    //initialisation du personnage
-    Perso perso;
-    perso.numSprite = 0;
-    chargerSpritesPerso(perso.numSprite,perso.Perso_Sprites);
-    perso.position.x=FENETRE_W/2;
-    perso.position.y=FENETRE_H/2;
-    // initialisation pokemon et stats
-    poke1.id = 3;
-    poke1.niv = 30;
-    poke1.exp = (poke1.niv)*(poke1.niv)*(poke1.niv);
-    printf("%d\n",poke1.exp);
-    poke2.id = 2;
-    poke2.niv = 20;
-    calcul_stat(&poke2);
-    calcul_stat(&poke1);
-    int attaques[4]={5,12,3,6};
-    charger_att((poke1.attaque), attaques);
-    //la boucle suivante constitue le menu
-    int continuer = 1;
-    int Menu=0;
-    SDL_Event event; //on crée un evenement
-    //intro_video(ecran);
-    while (continuer){
-        Menu = menu(ecran);
-        switch(Menu){
-            case -2:
-              continuer = 0;
-            break;
-            case 0: //Quitter
-                  continuer = 0;
-            break;
-            case 1: //Alexis/Sarah
-=======
 
 int main(int argc, char** argv)
 {
@@ -116,7 +64,6 @@ int main(int argc, char** argv)
       case 0: //Quitter
         continuer = 0;
         break;
->>>>>>> Alexis
 
       case 1: //Alexis/Sarah
         return 0;
